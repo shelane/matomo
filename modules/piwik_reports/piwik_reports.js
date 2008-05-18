8658fc402d1591a5faac2388890536f689982140
hass <hass@85918.no-reply.drupal.org>
2008-05-18 23:28:52 +0000

    * Show data table first and then add data * Show "Loading data..." message while loading data from remote Piwik server

--- modules/piwik_reports/piwik_reports.js
+++ modules/piwik_reports/piwik_reports.js
@@ -1,37 +1,51 @@
 $(document).ready(function(){
   var url = $("#edit-url").val();
   var page = $("#edit-page").val();
-  $.getJSON(url,
-    function(data){
-      var header = "<table class='sticky-enabled sticky-table'><thead class='tableHeader-processed'><tr><th>" + Drupal.t('Label') + "</th>";
-      if (page == "websites") {
-        header += "<th>" + Drupal.t('Visitors') + "</th>";
-      }
+
+  // Build HTML for data table.
+  var columns = 2;
+  var header = "<table class='sticky-enabled sticky-table'><thead class='tableHeader-processed'>";
+  header += "<tr><th>" + Drupal.t('Label') + "</th>";
+  if (page == "websites") {
+    header += "<th>" + Drupal.t('Unique visitors') + "</th>";
+  }
+  if (page == "actions") {
+    header += "<th>" + Drupal.t('Unique visitors') + "</th><th>" + Drupal.t('Hits') + "</th>";
+    columns = 3;
+  }
+  if (page == "search") {
+    header += "<th>" + Drupal.t('Visits') + "</th>";
+  }
+  header += "</tr></thead><tbody></tbody></table>";
+
+  // Add the table and show "Loading data..." status message for long running requests.
+  $("#pagestable").html(header);
+  $("#pagestable > table > tbody").html("<tr class='odd'><td colspan='" + columns + "'>" + Drupal.t('Loading data...') + "</td></tr>");
+
+  // Get data from remote Piwik server.
+  $.getJSON(url, function(data){
+    var content = "";
+    var tr_class = "even";
+
+    $.each(data, function(i,item){
+      if (tr_class == "odd") { item_class = "even"; } else { item_class = "odd"; }
+      tr_class = item_class;
+
+      content += "<tr class='" + item_class + "'><td>" + item["label"] + "</td>";
       if (page == "actions") {
-        header += "<th>" + Drupal.t('Visitors') + "</th><th>" + Drupal.t('Hits') + "</th>";
+        content += "<td>" + item["nb_uniq_visitors"] + "</td><td>" + item["nb_hits"] + "</td>";
+      }
+      if (page == "websites") {
+        content += "<td>" + item["nb_unique_visitors"] + "</td>";
       }
       if (page == "search") {
-        header += "<th>" + Drupal.t('Visits') + "</th>";
+        content += "<td>" + item["nb_visits"] + "</td>";
       }
-      header += "</tr></thead><tbody>";
-      var content = "";
-      var footer = "</table>";
-      var tr_class = "even";
-      $.each(data, function(i,item){
-        if (tr_class == "odd") { item_class = "even"; } else { item_class = "odd"; }      
-        tr_class = item_class;
-        content += "<tr class='" + item_class + "'><td>" + item["label"] + "</td>";
-        if (page == "actions") {
-          content += "<td>" + item["nb_uniq_visitors"] + "</td><td>" + item["nb_hits"] + "</td>";
-        }
-        if (page == "websites") {
-          content += "<td>" + item["nb_unique_visitors"] + "</td>";
-        }
-        if (page == "search") {
-          content += "<td>" + item["nb_visits"] + "</td>";
-        }
-        content += "</tr></tbody>";
-      });
-      $("#pagestable").html('<br />' + header + content + footer);
+      content += "</tr>";
     });
+
+    // Push data into table and replace "Loading data..." status message.
+    $("#pagestable > table > tbody").html(content);
+  });
+
 });
