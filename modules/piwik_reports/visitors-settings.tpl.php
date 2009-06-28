77d8e9a0b1394e1240e0948bbc4ce371f586939d
hass <hass@85918.no-reply.drupal.org>
2009-06-28 19:46:51 +0000

    Kick cross domain policy away.

--- modules/piwik_reports/visitors-settings.tpl.php
+++ modules/piwik_reports/visitors-settings.tpl.php
@@ -13,7 +13,7 @@
 <h2><?php print t('Configurations') ?></h2>
 <div class="content">
   <object width="100%" height="250" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?v2i" id="UserSettingsgetConfigurationChart">
-    <param name="allowScriptAccess" value="sameDomain"/>
+    <param name="allowScriptAccess" value="always"/>
     <param name="wmode" value="opaque"/>
     <param name="flashvars" value="data-file=<?php print $data2_url ?>"/>
   </object>
@@ -22,7 +22,7 @@
 <h2><?php print t('Operating systems') ?></h2>
 <div class="content">
   <object width="100%" height="250" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?v2i" id="UserSettingsgetOSChart">
-    <param name="allowScriptAccess" value="sameDomain"/>
+    <param name="allowScriptAccess" value="always"/>
     <param name="wmode" value="opaque"/>
     <param name="flashvars" value="data-file=<?php print $data3_url ?>"/>
   </object>
@@ -31,7 +31,7 @@
 <h2><?php print t('Screen resolutions') ?></h2>
 <div class="content">
   <object width="100%" height="250" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?v2i" id="UserSettingsgetResolutionChart">
-    <param name="allowScriptAccess" value="sameDomain"/>
+    <param name="allowScriptAccess" value="always"/>
     <param name="wmode" value="opaque"/>
     <param name="flashvars" value="data-file=<?php print $data4_url ?>"/>
   </object>
