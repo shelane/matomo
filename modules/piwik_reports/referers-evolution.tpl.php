82bad52a4daf6a989e404f964dd449a271063807
hass <hass@85918.no-reply.drupal.org>
2010-10-09 15:20:08 +0000

    Rollback #433922: Commit was a fault...

---
+++ modules/piwik_reports/referers-evolution.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+?>
+<div class="content">
+  <object width="100%" height="300" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?v2i" id="ReferersgetLastDirectEntryGraphChart">
+    <param name="allowScriptAccess" value="always"/>
+    <param name="wmode" value="opaque"/>
+    <param name="flashvars" value="data-file=<?php print $data1_url ?>"/>
+  </object>
+</div>
