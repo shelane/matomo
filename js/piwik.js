6ff997435f749f93eadb56e5892da8601027368d
hass <hass@85918.no-reply.drupal.org>
2015-10-31 12:31:14 +0100

    Migrate piwik.js and make eslint

--- js/piwik.js
+++ js/piwik.js
@@ -1,32 +1,34 @@
-(function ($) {
+(function ($, drupalSettings) {
 
-$(document).ready(function() {
+  "use strict";
 
-  // Attach mousedown, keyup, touchstart events to document only and catch
-  // clicks on all elements.
-  $(document.body).bind("mousedown keyup touchstart", function(event) {
+  $(document).ready(function() {
 
-    // Catch the closest surrounding link of a clicked element.
-    $(event.target).closest("a,area").each(function() {
+    // Attach mousedown, keyup, touchstart events to document only and catch
+    // clicks on all elements.
+    $(document.body).bind("mousedown keyup touchstart", function(event) {
 
-      if (Drupal.settings.piwik.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
-        // Mailto link clicked.
-        _paq.push(["trackEvent", "Mails", "Click", this.href.substring(7)]);
-      }
+      // Catch the closest surrounding link of a clicked element.
+      $(event.target).closest("a,area").each(function() {
 
+        if (drupalSettings.piwik.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
+          // Mailto link clicked.
+          _paq.push(["trackEvent", "Mails", "Click", this.href.substring(7)]);
+        }
+
+      });
     });
+
+    // Colorbox: This event triggers when the transition has completed and the
+    // newly loaded content has been revealed.
+    $(document).bind("cbox_complete", function () {
+      var href = $.colorbox.element().attr("href");
+      if (href) {
+        _paq.push(["setCustomUrl", href]);
+        _paq.push(["trackPageView"]);
+      }
+    });
+
   });
 
-  // Colorbox: This event triggers when the transition has completed and the
-  // newly loaded content has been revealed.
-  $(document).bind("cbox_complete", function () {
-    var href = $.colorbox.element().attr("href");
-    if (href) {
-      _paq.push(["setCustomUrl", href]);
-      _paq.push(["trackPageView"]);
-    }
-  });
-
-});
-
-})(jQuery);
+})(jQuery, drupalSettings);
